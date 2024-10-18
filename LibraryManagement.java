9539f069388795c2873990258c4b1297bef933a8
Mohanraj S <136877362+Mohan1605@users.noreply.github.com>
2024-10-18 18:20:40 +0530

    Add files via upload

---
+++ LibraryManagement.java
@@ -0,0 +1,16 @@
+
+package week1.homeassignment;
+
+public class LibraryManagement {
+	
+	public static void main(String[] args) 
+	{
+		Library book = new Library();
+		book.addbook("Science");
+		book.issueBook();
+	
+	
+	
+	}
+
+}
