8dc3117e9222764732ec1cf3522701b981d7ae1d
Mohanraj S <136877362+Mohan1605@users.noreply.github.com>
2024-10-18 12:16:14 +0530

    Add files via upload

---
+++ Chrome.java
@@ -0,0 +1,26 @@
+package week1.homeassignment;
+
+public class Chrome {
+	public static void main(String[] args) {
+		
+		String name = "google";
+		boolean isBeta = false;
+		float value = 0.9f;
+		int releaseyear = 2008;
+		char logo = 'c';
+		
+		
+		
+		
+		System.out.println(name);
+		System.out.println(isBeta);
+		System.out.println(value);
+		System.out.println(releaseyear);
+		System.out.println(logo);
+		
+	
+		
+		
+	}
+
+}
